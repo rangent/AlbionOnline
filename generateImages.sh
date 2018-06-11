ec5a00ed60551894cd46fb606392830d87dd8207
rangent <rangent@yahoo.com>
2018-06-11 13:46:29 -0600

    Adding opencvDirectinstall.sh

--- generateImages.sh
+++ generateImages.sh
@@ -10,6 +10,6 @@ do
 	file=$(basename "$file")
 	echo "$file"
 	convert /media/sf_Screenshots/raw/"$file" -crop 420x28+1079+419 -colorspace Gray -brightness-contrast 30x100 /media/sf_Screenshots/processed/"$file".title.png
-	convert /media/sf_Screenshots/raw/"$file" -crop 614x68+943+626 -negate /media/sf_Screenshots/processed/"$file".price.png
+	convert /media/sf_Screenshots/raw/"$file" -crop 614x68+943+626 -negate -modulate 100,200,100 /media/sf_Screenshots/processed/"$file".price.png
 	convert /media/sf_Screenshots/processed/"$file".title.png /media/sf_Screenshots/space.png /media/sf_Screenshots/processed/"$file".price.png -background black -append /media/sf_Screenshots/rekognition/"$file".png
 done
